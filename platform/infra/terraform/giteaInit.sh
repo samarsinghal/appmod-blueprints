df716b7ec2fb1b9edef98e4dfca137ebd4857ad1
hmuthusamy <hmmuthus@amazon.com>
2024-09-23 18:51:12 -0400

    Fixed Gitea script issues

--- platform/infra/terraform/giteaInit.sh
+++ platform/infra/terraform/giteaInit.sh
@@ -7,9 +7,11 @@ export DOMAIN_NAME=$(kubectl get service ingress-nginx-controller -n ingress-ngi
 
 export REPO_ROOT=$(git rev-parse --show-toplevel)
 echo "Creating app repositories in gitea"
-apk add gitea
-apk add git
-apk add openssh-client
+
+# Install Binaries below if needed
+# apk add gitea
+# apk add git
+# apk add openssh-client
 
 cd ${REPO_ROOT}/applications
 
@@ -23,40 +25,52 @@ curl -k -X POST "https://$DOMAIN_NAME/gitea/api/v1/admin/users/$USERNAME/repos" 
 curl -k -X POST "https://$DOMAIN_NAME/gitea/api/v1/admin/users/$USERNAME/repos" -H "content-type: application/json" -H "Authorization: Basic $ENCODED_USER_PASS" --data '{"name":"java"}'
 curl -k -X POST "https://$DOMAIN_NAME/gitea/api/v1/admin/users/$USERNAME/repos" -H "content-type: application/json" -H "Authorization: Basic $ENCODED_USER_PASS" --data '{"name":"terraform-eks"}'
 
-cd dotnet
-git init
 git config --global credential.helper store
-git checkout -b main
+mkdir -p ${REPO_ROOT}/applications/gitea
+cd ${REPO_ROOT}/applications/gitea
+git clone -c http.sslVerify=false https://$USER_PASS@$DOMAIN_NAME/gitea/$USERNAME/dotnet.git
+cd dotnet
+cp -r ${REPO_ROOT}/applications/dotnet ${REPO_ROOT}/applications/gitea/
 git add .
 git -c http.sslVerify=false commit -m "first commit" --no-verify
+git remote remove origin
 git remote add origin https://$DOMAIN_NAME/gitea/$USERNAME/dotnet.git
 git -c http.sslVerify=false push -u origin main --no-verify
 
-cd ../golang
-git init
-git checkout -b main
+cd ..
+git clone -c http.sslVerify=false https://$USER_PASS@$DOMAIN_NAME/gitea/$USERNAME/java.git
+cd java
+cp -r ${REPO_ROOT}/applications/java ${REPO_ROOT}/applications/gitea/
 git add .
 git -c http.sslVerify=false commit -m "first commit" --no-verify
-git remote add origin https://$DOMAIN_NAME/gitea/$USERNAME/golang.git
-git -c http.sslVerify=false push -u origin main --no-verify
-
-cd ../java
-git init
-git checkout -b main
-git add .
-git -c http.sslVerify=false commit -m "first commit" --no-verify
+git remote remove origin
 git remote add origin https://$DOMAIN_NAME/gitea/$USERNAME/java.git
 git -c http.sslVerify=false push -u origin main --no-verify
 
-cd ../../platform/infra/terraform
-git init
-git checkout -b main
-git add prod
-git add dev
-git add .gitignore
-git add create-cluster.sh
+cd ..
+git clone -c http.sslVerify=false https://$USER_PASS@$DOMAIN_NAME/gitea/$USERNAME/golang.git
+cd golang
+cp -r ${REPO_ROOT}/applications/golang ${REPO_ROOT}/applications/gitea/
+git add .
 git -c http.sslVerify=false commit -m "first commit" --no-verify
+git remote remove origin
+git remote add origin https://$DOMAIN_NAME/gitea/$USERNAME/golang.git
+git -c http.sslVerify=false push -u origin main --no-verify
+
+cd ..
+git clone -c http.sslVerify=false https://$USER_PASS@$DOMAIN_NAME/gitea/$USERNAME/terraform-eks.git
+cd terraform-eks
+cp -r ${REPO_ROOT}/platform/infra/terraform/dev ${REPO_ROOT}/applications/gitea/terraform-eks/
+cp -r ${REPO_ROOT}/platform/infra/terraform/prod ${REPO_ROOT}/applications/gitea/terraform-eks/
+cp ${REPO_ROOT}/platform/infra/terraform/.gitignore ${REPO_ROOT}/applications/gitea/terraform-eks/
+cp ${REPO_ROOT}/platform/infra/terraform/create-cluster.sh  ${REPO_ROOT}/applications/gitea/terraform-eks/
+git add .
+git -c http.sslVerify=false commit -m "first commit" --no-verify
+git remote remove origin
 git remote add origin https://$DOMAIN_NAME/gitea/$USERNAME/terraform-eks.git
 git -c http.sslVerify=false push -u origin main --no-verify
+
+# cleanup temp gitea folder
+rm -rf ${REPO_ROOT}/applications/gitea
 
 echo "Copied all repositories to Gitea"
