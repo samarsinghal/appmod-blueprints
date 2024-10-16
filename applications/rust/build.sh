bbd29126f87bebab66e17a94b3fa7c7a533a36d3
shapirov <shapirov@amazon.com>
2024-10-15 23:05:50 -0400

    minor build script changes for rust

--- applications/rust/build.sh
+++ applications/rust/build.sh
@@ -1,6 +1,11 @@
+AWS_REGION=us-west-2
+CODEBUILD_RESOLVED_SOURCE_VERSION=latest
+AWS_ACCOUNT_ID=929819487611
+ECR_REGISTRY=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+ECR_REPOSITORY=moderneng/rust-microservice
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_REGISTRY
-docker build -t $ECR_REGISTRY/app/rust-microservice:$CODEBUILD_RESOLVED_SOURCE_VERSION .
-docker push $ECR_REGISTRY/app/rust-microservice:$CODEBUILD_RESOLVED_SOURCE_VERSION
-cd integration
-docker build -f Dockerfile -t $ECR_REGISTRY/$ECR_REPOSITORY:$CODEBUILD_RESOLVED_SOURCE_VERSION .d
-docker push $ECR_REGISTRY/$ECR_REPOSITORY:$CODEBUILD_RESOLVED_SOURCE_VERSION
+docker build -f Dockerfile --no-cache --platform linux/amd64 -t $ECR_REGISTRY/moderneng/rust-microservice:$CODEBUILD_RESOLVED_SOURCE_VERSION .
+docker push $ECR_REGISTRY/moderneng/rust-microservice:$CODEBUILD_RESOLVED_SOURCE_VERSION
+#cd integration
+#docker build -f Dockerfile -t $ECR_REGISTRY/$ECR_REPOSITORY:$CODEBUILD_RESOLVED_SOURCE_VERSION .
+#docker push $ECR_REGISTRY/$ECR_REPOSITORY:$CODEBUILD_RESOLVED_SOURCE_VERSION
