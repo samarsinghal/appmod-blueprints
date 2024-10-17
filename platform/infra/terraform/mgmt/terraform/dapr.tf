e343a0ffba095a323cd591bde71ef18e6ace87f8
Elamaran Shanmugam <elamaran.shan@gmail.com>
2024-10-17 15:26:27 -0400

    Adding DAPR Integration

--- platform/infra/terraform/mgmt/terraform/dapr.tf
+++ platform/infra/terraform/mgmt/terraform/dapr.tf
@@ -1,4 +1,4 @@
-resource "kubectl_manifest" "application_dapr" {
+resource "kubectl_manifest" "application_dapr_system" {
   yaml_body = templatefile("${path.module}/templates/argocd-apps/dapr.yaml", {
       GITHUB_URL = "https://dapr.github.io/helm-charts/"
     }
