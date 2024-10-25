7a144537acc9439ba71ea99e5ec99bcdca39db8f
Elamaran Shanmugam <elamaran.shan@gmail.com>
2024-10-25 09:09:09 -0400

    Usage Tracking

---
+++ platform/infra/terraform/mgmt/terraform/mgmt-cluster/variables.tf
@@ -0,0 +1,10 @@
+
+################################################################################
+# Usage Telemetry
+################################################################################
+
+variable "observability_tag" {
+  description = "Tag to identify Modern Engineering on AWS usage within observability tools"
+  type        = string
+  default     = "qs-1ubotj5kl"
+}
