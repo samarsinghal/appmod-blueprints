7f79c089fbea598c9ddff52ffa1424914545490a
Zach Jacobson <zjacobso@amazon.com>
2024-11-08 13:45:27 -0800

    workflow templates

--- platform/infra/terraform/mgmt/terraform/argo-events.tf
+++ platform/infra/terraform/mgmt/terraform/argo-events.tf
@@ -1,6 +1,6 @@
 resource "kubectl_manifest" "applications-argocd-argo-events" {
   yaml_body = templatefile("${path.module}/templates/argocd-apps/argo-events.yaml", {
-    GITHUB_URL = local.repo_url
+    GITHUB_URL = "https://argoproj.github.io/argo-helm"
     }
   )
 }
