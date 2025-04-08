02f7a7d3c3b7805edd60a2598fb6f1b8407c2a7a
hmuthusamy <hmmuthus@amazon.com>
2025-04-08 15:21:09 -0400

    Fixed versions for ACK and ingress killer issue

--- platform/infra/terraform/mgmt/terraform/ack.tf
+++ platform/infra/terraform/mgmt/terraform/ack.tf
@@ -29,10 +29,8 @@ resource "kubectl_manifest" "application_argocd_ack" {
   )
 
   provisioner "local-exec" {
-    when = create
-
-    command = "kubectl wait --for=jsonpath=.status.health.status=Healthy -n argocd application/ack --timeout=300s &&  kubectl wait --for=jsonpath=.status.sync.status=Synced --timeout=300s -n argocd application/ack"
-
+    command = "kubectl wait --for=jsonpath=.status.health.status=Healthy -n argocd application/ack --timeout=900s && kubectl wait --for=jsonpath=.status.sync.status=Synced --timeout=900s -n argocd application/ack"
+    
     interpreter = ["/bin/bash", "-c"]
   }
 }
